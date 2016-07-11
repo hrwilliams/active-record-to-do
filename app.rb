f548770d995089baad3416902bc5b58cc9fbcb01
Alexander Jacks and Brian Grant <brian@>
2016-07-11 16:30:05 -0700

    added views and integration spec

--- app.rb
+++ app.rb
@@ -5,11 +5,23 @@ require('./lib/task')
 require('./lib/list')
 require 'pg'
 
-DB = PG.connect({:dbname => "to_do"})
+DB = PG.connect({:dbname => "to_do_test"})
 
 get('/') do
-  @tasks = Task.all()
   erb(:index)
+end
+get('/lists/new') do
+  erb(:list_form)
+end
+get('/lists') do
+  @lists = List.all()
+  erb(:lists)
+end
+post('/lists') do
+  name = params.fetch("name")
+  list = List.new({:name => name, :id => nil})
+  list.save()
+  erb(:list_success)
 end
 
 post('/tasks') do
